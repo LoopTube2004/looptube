e89a141394eee1f8f9abae48865f30bbe426a80e
nguy3ntrann <phucnguyentran79@gmail.com>
2024-03-13 13:01:37 -0400

    youtube link validator done + unit tests

---
+++ client/src/scripts/ytvalidator.js
@@ -0,0 +1,36 @@
+// Reference: https://www.reddit.com/r/learnprogramming/comments/7ioxra/how_to_check_if_a_youtube_link_is_valid_or_not/
+
+const axios = require("axios");
+
+async function validateYouTubeLink(youtube_url) {
+  const url = `https://www.youtube.com/oembed?format=json&url=${youtube_url}`;
+
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      console.log("Valid Youtube link");
+    } else {
+      console.log("Not a valid Youtube link");
+    }
+  } catch (error) {
+    console.log(`Not a valid youtube link ${error}`);
+  }
+}
+
+// Example usage
+const test = [
+  "https://www.youtube.com/watch?v=1vVvDdBnCos", //ok
+  "https://www.youtube.com/watch?v=LAraqZvY6KY", //OK
+  "https://www.youtube.com/watch?v=1", //not ok
+  "https://www.youtube.com/watch?v=1vVvDdBnCoa", //not ok
+  "https://www.youtube.com/watch?v=1vVDvdBnCos", //not ok
+  "https://www.youtube.com/watch?v=1vVDdBnCos", //not ok
+];
+
+async function testYouTubeLinks(links) {
+  for (let i = 0; i < links.length; i++) {
+    await validateYouTubeLink(links[i]);
+  }
+}
+
+testYouTubeLinks(test);
