721cb613b5e750e31bcd0a0a729d12a3d49604a2
nguy3ntrann <phucnguyentran79@gmail.com>
2024-04-14 18:17:13 -0400

    passing state of VideoForm from Home to IFrame

--- client/src/components/VideoForm.js
+++ client/src/components/VideoForm.js
@@ -5,64 +5,10 @@ import { validateYouTubeLink } from "../scripts/YoutubeValidator";
 import { getIdYoutubeVideo } from "../scripts/FetchYoutubeData";
 import IFrame from "./IFrame";
 
-const VideoForm = () => {
-  const [url, setUrl] = useState("");
-  const [startHour, setStartHour] = useState(0);
-  const [startMinute, setStartMinute] = useState(0);
-  const [startSecond, setStartSecond] = useState(0);
-  const [endHour, setEndHour] = useState(0);
-  const [endMinute, setEndMinute] = useState(0);
-  const [endSecond, setEndSecond] = useState(0);
-  const [customize, setCustomize] = useState(0);
-  const [errors, setErrors] = useState([]);
-  const [noErrors, setNoErrors] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let tempErrors = []; // Temporary array to collect error messages
-
-    //we need + before startMinute to convert it to a number, now it is a string instead
-    var startTime = startHour * 3600 + (+startMinute) * 60 + (+startSecond);
-    var endTime = endHour * 3600 + (+endMinute) * 60 + (+endSecond);
-
-    //console.log(startTime + " " + endTime)
-
-    const checkValidUrl = await validateYouTubeLink(url);
-
-    if (!checkValidUrl) {
-      tempErrors.push("The URL is not a valid YouTube link.");
-    }
-
-    const youtubeLength = await FetchYoutubeData(url);
-
-    // Collecting error messages based on different conditions
-    if (!url) {
-      tempErrors.push("Your URL is missing.");
-    }
-    if (endTime < startTime) {
-      //console.log("End time should be greater than start time.")
-      tempErrors.push("End time should be greater than start time.");
-    }
-    if (endTime > youtubeLength || startTime > youtubeLength) {
-      tempErrors.push("Start time or end time exceeds the video length.");
-    }
-
-    // Update the error state with all collected error messages
-    setErrors(tempErrors);
-
-    // Check if there are any errors before proceeding
-    if (tempErrors.length === 0) {
-      // Set noError state to false. This will help indicating whether we render a video later.
-      setNoErrors(true);
-    } else {
-      // Errors are present, handle them appropriately
-      console.log("Errors are:", tempErrors);
-    }
-  };
-
+const VideoForm = (props) => {
   return (
     <div>
-      <form className={styles["video-form"]} onSubmit={handleSubmit}>
+      <form className={styles["video-form"]} onSubmit={props.handleSubmit}>
         <h1>LoopTube</h1>
         <h3>Fill your form</h3>
 
@@ -71,8 +17,8 @@ const VideoForm = () => {
           <label>Youtube Url</label>
           <input
             type="text"
-            onChange={(e) => setUrl(e.target.value)}
-            value={url}
+            onChange={(e) => props.setUrl(e.target.value)}
+            value={props.url}
             placeholder="Enter your youtube url here..."
           />
         </div>
@@ -82,24 +28,24 @@ const VideoForm = () => {
             type="number"
             min="0" //So the value of input cannot be negative
             step="1" //only accept integer
-            onChange={(e) => setStartHour(e.target.value)}
-            value={startHour}
+            onChange={(e) => props.setStartHour(e.target.value)}
+            value={props.startHour}
           />
           <span>:</span>
           <input
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setStartMinute(e.target.value)}
-            value={startMinute}
+            onChange={(e) => props.setStartMinute(e.target.value)}
+            value={props.startMinute}
           />
           <span>:</span>
           <input
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setStartSecond(e.target.value)}
-            value={startSecond}
+            onChange={(e) => props.setStartSecond(e.target.value)}
+            value={props.startSecond}
           />
         </div>
         <div className={styles["time-input-group"]}>
@@ -108,24 +54,24 @@ const VideoForm = () => {
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setEndHour(e.target.value)}
-            value={endHour}
+            onChange={(e) => props.setEndHour(e.target.value)}
+            value={props.endHour}
           />
           <span>:</span>
           <input
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setEndMinute(e.target.value)}
-            value={endMinute}
+            onChange={(e) => props.setEndMinute(e.target.value)}
+            value={props.endMinute}
           />
           <span>:</span>
           <input
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setEndSecond(e.target.value)}
-            value={endSecond}
+            onChange={(e) => props.setEndSecond(e.target.value)}
+            value={props.endSecond}
           />
         </div>
         <div className={styles["input-group"]}>
@@ -134,8 +80,8 @@ const VideoForm = () => {
             type="number"
             min="0"
             step="1"
-            onChange={(e) => setCustomize(e.target.value)}
-            value={customize}
+            onChange={(e) => props.setCustomize(e.target.value)}
+            value={props.customize}
           />
         </div>
 
@@ -143,23 +89,14 @@ const VideoForm = () => {
           <button type="submit">Submit</button>
         </div>
 
-        {errors.length > 0 && (
+        {props.errors.length > 0 && (
           <div>
-            {errors.map((error, index) => (
+            {props.errors.map((error, index) => (
               <p key={index}>{error}</p>
             ))}
           </div>
         )}
       </form>
-
-      {noErrors && (
-        <IFrame
-          youtubeID={getIdYoutubeVideo(url)}
-          start={startHour * 3600 + (+startMinute) * 60 + (+startSecond)}
-          end={endHour * 3600 + (+endMinute) * 60 + (+endSecond)}
-          time={customize}
-        />     
-      )}
     </div>
   );
 };
