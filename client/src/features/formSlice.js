f202a3b00b1507efc5f1ee9c3a6ba99171e7c469
nguy3ntrann <phucnguyentran79@gmail.com>
2024-04-24 17:35:19 -0400

    use redux to pass form state

---
+++ client/src/features/formSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  startHour: 0,
+  startMinute: 0,
+  startSecond: 0,
+  endHour: 0,
+  endMinute: 0,
+  endSecond: 0,
+  url: null,
+  customize: 0,
+};
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    updateStartHour: (state, action) => {
+      state.startHour = action.payload;
+    },
+    updateStartMinute: (state, action) => {
+      state.startMinute = action.payload;
+    },
+    updateStartSecond: (state, action) => {
+      state.startSecond = action.payload;
+    },
+    updateEndHour: (state, action) => {
+      state.endHour = action.payload;
+    },
+    updateEndMinute: (state, action) => {
+      state.endMinute = action.payload;
+    },
+    updateEndSecond: (state, action) => {
+      state.endSecond = action.payload;
+    },
+    updateUrl: (state, action) => {
+      state.url = action.payload;
+    },
+    updateCustomize: (state, action) => {
+      state.customize = action.payload;
+    },
+  },
+});
+
+export const {
+  updateStartHour,
+  updateStartMinute,
+  updateStartSecond,
+  updateEndHour,
+  updateEndMinute,
+  updateEndSecond,
+  updateUrl,
+  updateCustomize,
+} = formSlice.actions;
+
+export default formSlice.reducer;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,30 +5,31 @@ import { useState } from "react";
 import { FetchYoutubeData, getIdYoutubeVideo } from "../scripts/FetchYoutubeData";
 import { validateYouTubeLink } from "../scripts/YoutubeValidator";
 import toast from "react-hot-toast";
-
+import { useSelector } from "react-redux";
 
 const Home = () => {
-    const [url, setUrl] = useState("");
-    const [startHour, setStartHour] = useState(0);
-    const [startMinute, setStartMinute] = useState(0);
-    const [startSecond, setStartSecond] = useState(0);
-    const [endHour, setEndHour] = useState(0);
-    const [endMinute, setEndMinute] = useState(0);
-    const [endSecond, setEndSecond] = useState(0);
-    const [customize, setCustomize] = useState(0);
     const [errors, setErrors] = useState([]);
     const [noErrors, setNoErrors] = useState(false);
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(0);
     const [youtubeID, setYoutubeID] = useState("");
 
+    const startHour = useSelector((state) => state.form.startHour);
+    const startMinute = useSelector((state) => state.form.startMinute);
+    const startSecond = useSelector((state) => state.form.startSecond);
+    const endHour = useSelector((state) => state.form.endHour);
+    const endMinute = useSelector((state) => state.form.endMinute);
+    const endSecond = useSelector((state) => state.form.endSecond);
+    const url = useSelector((state) => state.form.url);
+    const customize = useSelector((state) => state.form.customize);
+
     const handleSubmit = async (e) => {
       e.preventDefault();
       let tempErrors = []; // Temporary array to collect error messages
   
       //we need + before startMinute to convert it to a number, now it is a string instead
-      var startTime = startHour * 3600 + (+startMinute) * 60 + (+startSecond);
-      var endTime = endHour * 3600 + (+endMinute) * 60 + (+endSecond);
+      var startTime = (+startHour) * 3600 + (+startMinute) * 60 + (+startSecond);
+      var endTime = (+endHour) * 3600 + (+endMinute) * 60 + (+endSecond);
   
       //console.log(startTime + " " + endTime)
   
@@ -77,22 +78,6 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <VideoForm 
-        url={url}
-        setUrl={setUrl}
-        startHour={startHour}
-        setStartHour={setStartHour}
-        startMinute={startMinute}
-        setStartMinute={setStartMinute}
-        startSecond={startSecond}
-        setStartSecond={setStartSecond}
-        endHour={endHour}
-        setEndHour={setEndHour}
-        endMinute={endMinute}
-        setEndMinute={setEndMinute}
-        endSecond={endSecond}
-        setEndSecond={setEndSecond}
-        customize={customize}
-        setCustomize={setCustomize}
         handleSubmit={handleSubmit}
         errors={errors}
       />
