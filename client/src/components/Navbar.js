f3087a4f3bc261c0888f4de9b203d728d30817e4
peterong <onggiaphuoc2004@gmail.com>
2024-04-25 14:39:05 -0400

    finish set up for logout and login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './Navbar.module.css'; // Import the CSS module
 import LogoutButton from './LogoutButton';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function Navbar() {
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user.user)
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
         {/* Other nav items can be placed here */}
-        <div className={styles.logoutButton}>
+        {user && <div className={styles.logoutButton}>
           <LogoutButton />
-        </div>
+        </div>}
       </div>
     </nav>
   );
