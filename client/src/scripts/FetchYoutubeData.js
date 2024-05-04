b9382e78062c953598e424ed56f34fd7f7987617
peterong <onggiaphuoc2004@gmail.com>
2024-05-04 18:42:56 -0400

    fix the synchronize.py, update ux/ui of google extension form

--- client/src/scripts/FetchYoutubeData.js
+++ client/src/scripts/FetchYoutubeData.js
@@ -1,27 +1,35 @@
-
-import axios from 'axios'
-
-
-export const FetchYoutubeData = async (url) => { //Fetch youtube data
-    const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY
+export const FetchYoutubeData = async (url) => { // Fetch YouTube data
+    var apiKey = ''
+    fetch('https://9d6ss4k11k.execute-api.us-east-2.amazonaws.com/default/looptube_yt_api', {
+        method: 'post',
+    })
+    .then(response => response.text())
+    .then(data => {
+        apiKey = data;
+    });
     const videoId = getIdYoutubeVideo(url)
     const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=contentDetails&key=${apiKey}`
     
     let youtubeLength = 0 
     try {
-        const response = await axios.get(apiUrl)
-        const time = response.data.items[0].contentDetails.duration
-        youtubeLength = iso8601DurationToSeconds(time)
+        const response = await fetch(apiUrl)
+        if (response.ok) {
+            const data = await response.json()
+            const time = data.items[0].contentDetails.duration
+            youtubeLength = iso8601DurationToSeconds(time)
+        } else {
+            throw new Error('Failed to fetch data from YouTube API')
+        }
     } catch (error) {
         console.log("There is error with FetchYoutubeData.js")
         console.log(error)
-        return {error1: " Cannot fetch youtube video "}
+        return { error1: " Cannot fetch youtube video " }
     }
 
     return youtubeLength
 }
 
-export function iso8601DurationToSeconds(duration) { //Convert time iso 8601 format to seconds - source chatgpt
+export function iso8601DurationToSeconds(duration) { // Convert time ISO 8601 format to seconds
     let hours = 0, minutes = 0, seconds = 0;
 
     // Extract hours
@@ -45,11 +53,8 @@ export function iso8601DurationToSeconds(duration) { //Convert time iso 8601 for
     return hours * 3600 + minutes * 60 + seconds;
 }
 
-export function getIdYoutubeVideo(url) { //Get id of youtube url
-    const splitted = url.split("v=")[1]
-    const params = splitted.split("&")
-    const id =  params[0] 
-    return id 
+export function getIdYoutubeVideo(url) { // Get ID of YouTube URL
+    const splitted = url.split("v=")[1];
+    const params = splitted.indexOf("&") > -1 ? splitted.split("&") : [splitted];
+    return params[0];
 }
-
-
